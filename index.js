5d5f8b87347b24aa5e6ad9d2e47965913411f2b3
Irina Kagan <irinagamb@gmail.com>
2019-10-12 22:08:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const driver = {
 };
 function updateDriverWithKeyAndValue(driver,key,value){
   const newDriver = { ...driver};
+  newDriver[key] = value;
+  return newDriver;
 }
 function destructivelyUpdateDriverWithKeyAndValue(){
 
