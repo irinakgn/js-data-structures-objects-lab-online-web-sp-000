25c160590cf6332b6b83d5f596e437375177e8e7
Irina Kagan <irinagamb@gmail.com>
2019-10-12 22:15:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function destructivelyUpdateDriverWithKeyAndValue(obj,key,value){
 }
 
 function deleteFromDriverByKey(driver,key){
-  delete driver.key;
+  delete driver['key'];
 
 return driver;
 }
